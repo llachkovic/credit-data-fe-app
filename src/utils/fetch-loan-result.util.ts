86ab9ee409d886e31cf75f7c88aadee4095749ab
llachkovic <905667@student.uek.krakow.pl>
2024-05-20 15:18:18 +0200

    removed redundant try-catch statements

--- src/utils/fetch-loan-result.util.ts
+++ src/utils/fetch-loan-result.util.ts
@@ -42,53 +42,38 @@ export interface ResultResponse {
 }
 
 export const fetchNewLoanResult = async (payload: CreditApplicationForm) => {
-    try {
-        const response = await fetch('http://127.0.0.1:5000/predict', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(payload),
-        })
-        if (!response.ok) {
-            throw new Error('Failed to fetch data')
-        }
-        const data: ResultResponse = await response.json()
-        return data
-    } catch (error) {
-        console.error(error)
+    const response = await fetch('http://127.0.0.1:5000/predict', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+    })
+    if (!response.ok) {
+        throw new Error('Failed to fetch data')
     }
+    const data: ResultResponse = await response.json()
+    return data
 }
 
 export const fetchAllLoanResults = async () => {
-    try {
-        const response = await fetch('http://127.0.0.1:5000/results', {
-            method: 'GET',
-        })
-        if (!response.ok) {
-            throw new Error('Failed to fetch data')
-        }
-        const data: ResultResponse[] = await response.json()
-        return data
-    } catch (error) {
-        console.error(error)
+    const response = await fetch('http://127.0.0.1:5000/results', {
+        method: 'GET',
+    })
+    if (!response.ok) {
+        throw new Error('Failed to fetch data')
     }
+    const data: ResultResponse[] = await response.json()
+    return data
 }
 
 export const fetchLoanResult = async (resultId: string) => {
-    try {
-        const response = await fetch(
-            `http://127.0.0.1:5000/results/${resultId}`,
-            {
-                method: 'GET',
-            }
-        )
-        if (!response.ok) {
-            throw new Error('Failed to fetch data')
-        }
-        const data: ResultResponse = await response.json()
-        return data
-    } catch (error) {
-        console.error(error)
+    const response = await fetch(`http://127.0.0.1:5000/results/${resultId}`, {
+        method: 'GET',
+    })
+    if (!response.ok) {
+        throw new Error('Failed to fetch data')
     }
+    const data: ResultResponse = await response.json()
+    return data
 }
